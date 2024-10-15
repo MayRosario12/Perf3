f6a9b26a1fb46fcbe823166f770c19aaae10f880
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-15 21:14:29 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create an array called favSingers and store your three favorite singers in it.
+const favSingers = ['Taylor Swift', 'Beyonce', 'Adele'];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+const favNumbers = [1, 7, 13, 21];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true].
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
